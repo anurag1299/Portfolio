6ae54b00c77080a48477cf06e4f5ed2dc0e26fab
Alkatraz <anuragbmore@gmail.com>
2020-05-14 11:08:41 +0530

    added config

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteTitle: "Anurag More | Computer Science Engineer",
+  name: "Anurag More",
+  email: "anuragbmore@gmail.com",
+  github: "https://github.com/anurag1299",
+  socialMedial: [
+    {
+      name: "Github",
+      url: "https://github.com/anurag1299",
+    },
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/anurag-more-9aa67a19a/",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/ludum_elit",
+    },
+  ],
+
+  navHeight: 100,
+  colors: {
+    blue: "#007BFF",
+    navy: "#171C28",
+    lightNavy: "#8892B0",
+    lightestNavy: "#CCD6F6",
+  },
+};
