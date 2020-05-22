1d95daf543900411f33c28a7e71cb56344aace23
Alkatraz <anuragbmore@gmail.com>
2020-05-22 13:09:11 +0530

    looks like i solved it

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ const StyledMainContainer = styled(Main)`
   counter-reset: section;
 `;
 
-const Index = ({ allContentId }) => {
-  console.log(allContentId);
+const Index = ({ data }) => {
+  console.log(data);
   return (
     <Layout>
       <StyledMainContainer className="fillHeight">
@@ -27,18 +27,24 @@ export default Index;
 
 export async function getStaticProps() {
   Promise.resolve(getContent())
-    .then(
-      data=>Promise.resolve(data)
-      .then(data=>console.log("idata",data))
-      .catch(err=>console.log("ierr",err))
-      )
-    .catch(err=>console.log("ierr",err))
+    .then((data) => {
+      console.log("idata", data);
+      return data;
+    })
+    .then((data) =>
+      Promise.resolve(data)
+        .then((data) => {
+          console.log("mdata", data);
+          return { props: { data } };
+        })
+        .catch((err) => console.log(err))
+    )
+    .catch((err) => console.log("ierr", err));
+
   // const allContentData = await allContentId.map((i) => {
   //   getContent(i.id);
   // });
   return {
-    props: {
-      
-    },
+    props: { data: "hi" },
   };
 }
