1a61bfa5310cb957425d0500a8931e59618ce4b6
Alkatraz <anuragbmore@gmail.com>
2020-06-02 23:46:26 +0530

    build

---
+++ _next/static/bP0ogqRerywF6nbnK8-bf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/_error":[a],"/index":[a]}}("static\u002Fchunks\u002F0116455250436a843a2fb4532408afbd62bd3251.1d6043fbee897cdfec82.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
