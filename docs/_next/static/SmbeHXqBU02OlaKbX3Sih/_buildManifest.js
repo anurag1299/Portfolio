62d05c2623836bba850362451b6501cf3817f145
Alkatraz <anuragbmore@gmail.com>
2020-06-02 19:06:27 +0530

    fifth build

---
+++ docs/_next/static/SmbeHXqBU02OlaKbX3Sih/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/_error":[a],"/index":[a]}}("static\u002Fchunks\u002Fa22b69ada750d1e9e2f65050dee844f2d85890bc.1d6043fbee897cdfec82.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
